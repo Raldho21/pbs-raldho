1e5860b379772d4d2daafb8fc7fbec95a964787d
arel <raldhoarel@gmail.com>
2024-04-26 20:29:04 +0700

    add konfig varcel.json

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ const mysql = require('mysql');
 // Variabel koneksi database
 const db = mysql.createConnection({
   host: 'sql6.freesqldatabase.com',  // Alamat host database (biasanya 'localhost' jika database berjalan di mesin lokal)
-  user: 'sql6695751',       // Nama pengguna database
-  password: 'zhLByNj4RB',       // Kata sandi pengguna database (dalam contoh ini, password kosong)
-  database: 'sql6695751'  // Nama database yang akan digunakan (misalnya, 'db_penjualan')
+  user: 'sql6702197',       // Nama pengguna database
+  password: 'RsETY9pQ5Y',       // Kata sandi pengguna database (dalam contoh ini, password kosong)
+  database: 'sql6702197'  // Nama database yang akan digunakan (misalnya, 'db_penjualan')
 });
 
 // Ekspor variabel koneksi database untuk digunakan di modul lain
