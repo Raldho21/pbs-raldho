91ec71afbb228a9e3b7c75108d402a9506c4350a
raldho al yanrus <129926391+Raldho21@users.noreply.github.com>
2024-06-09 20:57:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 
 // Variabel koneksi database
 const db = mysql.createConnection({
-  host: 'sql6.freesqldatabase.com',  // Alamat host database (biasanya 'localhost' jika database berjalan di mesin lokal)
-  user: 'sql6702197',       // Nama pengguna database
-  password: 'RsETY9pQ5Y',       // Kata sandi pengguna database (dalam contoh ini, password kosong)
-  database: 'sql6702197'  // Nama database yang akan digunakan (misalnya, 'db_penjualan')
+  host: 'sql12.freesqldatabase.com',  // Alamat host database (biasanya 'localhost' jika database berjalan di mesin lokal)
+  user: 'sql12712838',       // Nama pengguna database
+  password: 'dYllccAMbK',       // Kata sandi pengguna database (dalam contoh ini, password kosong)
+  database: 'sql12712838'  // Nama database yang akan digunakan (misalnya, 'db_penjualan')
 });
 
 // Ekspor variabel koneksi database untuk digunakan di modul lain
